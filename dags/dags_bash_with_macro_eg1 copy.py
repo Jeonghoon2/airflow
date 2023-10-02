bc38ba648fe39e57bd283197772e3e08a1d42fef
Jeonghoon2 <ukidd12@naver.com>
2023-10-02 15:48:51 +0900

    Add Python Bash Operator With Macros

---
+++ dags/dags_bash_with_macro_eg1 copy.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_with_macro_eg1",
+    schedule="10 0 L * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    bash_task_1 = BashOperator(
+        task_id="bash_task_1",
+        env={'START_DATE':'{{ }}',
+             'END_DATE':'{{}}'
+            },
+            bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
+    )
