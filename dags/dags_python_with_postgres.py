bcc45a0a06ccf7bfdc279f0758bd74e9b972a705
Jeonghoon2 <ukidd12@naver.com>
2023-10-08 20:49:09 +0900

    Fix Connection info

--- dags/dags_python_with_postgres.py
+++ dags/dags_python_with_postgres.py
@@ -26,7 +26,7 @@ with DAG(
     insrt_postgres = PythonOperator(
         task_id = 'insrt_postgres',
         python_callable=insrt_postgres,
-        op_args=['localhost','5432','airflow','airflow','airflow']
+        op_args=['172.18.0.3','5432','airflow','airflow','airflow']
     )
 
     insrt_postgres
