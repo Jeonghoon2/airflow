47fa8367d1556660004a9759fd69d0929407998d
Jeonghoon2 <ukidd12@naver.com>
2023-10-10 16:19:37 +0900

    Add dags_python_with_postgres_hook_bulk_load

---
+++ dags/dags_python_with_postgres_hook_bulk_load.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+with DAG(
+        dag_id='dags_python_with_postgres_hook_bulk_load',
+        start_date=pendulum.datetime(2023, 4, 1, tz='Asia/Seoul'),
+        schedule=None,
+        catchup=False
+) as dag:
+    def insrt_postgres(postgres_conn_id, tbl_nm, file_nm, **kwargs):
+        postgres_hook = PostgresHook(postgres_conn_id)
+        postgres_hook.bulk_load(tbl_nm, file_nm)
+
+
+    insrt_postgres = PythonOperator(
+        task_id='insrt_postgres',
+        python_callable=insrt_postgres,
+        op_kwargs={'postgres_conn_id': 'conn-db-postres-custom',
+                   'tbl_nm': 'seoul_picture_bulk_1',
+                   'file_nm': '/opt/airflow/files/SemaPsgudInfoKorInfo/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/SemaPsgudInfoKorInfo'}
+    )
