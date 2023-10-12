02c86ce3b1927cae536cad4ec002900a853d1989
Jeonghoon2 <ukidd12@naver.com>
2023-10-12 18:19:43 +0900

    resave api to csv

--- dags/dags_seoul_api_picture.py
+++ dags/dags_seoul_api_picture.py
@@ -3,16 +3,16 @@ from airflow import DAG
 import pendulum
 
 with DAG(
-    dag_id='dags_seoul_api_picture',
-    schedule='0 7 * * *',
-    start_date=pendulum.datetime(2023,4,1, tz='Asia/Seoul'),
-    catchup=False
+        dag_id='dags_seoul_api_picture',
+        schedule='0 7 * * *',
+        start_date=pendulum.datetime(2023, 4, 1, tz='Asia/Seoul'),
+        catchup=False
 ) as dag:
     '''서울시립 미술 박물관 데이터 정보 '''
 
     tb_picture_info = SeoulApiToCsvOperator(
-        task_id = 'tb_picture_info',
-        dataset_nm = 'SemaPsgudInfoKorInfo',
+        task_id='tb_picture_info',
+        dataset_nm='SemaPsgudInfoKorInfo',
         path='/opt/airflow/files/SemaPsgudInfoKorInfo/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}',
         file_name='SemaPsgudInfoKorInfo.csv'
     )
