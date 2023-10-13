a005de6d91617b4b80dcd0c8439175666a863811
Jeonghoon2 <ukidd12@naver.com>
2023-10-13 17:47:21 +0900

    Fix task

--- dags/dags_external_task_sensor.py
+++ dags/dags_external_task_sensor.py
@@ -5,25 +5,15 @@ from datetime import timedelta
 from airflow.utils.state import State
 
 with DAG(
-    dag_id="dags_external_task_sensor",
-    start_date=pendulum.datetime(2023,4,1,tz='Asia/Seoul'),
-    schedule='0 7 * * *',
-    catchup=False
+        dag_id="dags_external_task_sensor",
+        start_date=pendulum.datetime(2023, 4, 1, tz='Asia/Seoul'),
+        schedule='0 7 * * *',
+        catchup=False
 ) as dag:
-
     external_task_sensor_a = ExternalTaskSensor(
         task_id='external_task_sensor_a',
         external_dag_id='dags_branch_python_operator',
         external_task_id='task_a',
-        allowed_states=[State.SKIPPED],
-        execution_delta=timedelta(hours=6),
-        poke_interval=10
-    )
-
-    external_task_sensor_b = ExternalTaskSensor(
-        task_id='external_task_sensor_b',
-        external_dag_id='dags_branch_python_operator',
-        external_task_id='task_b',
         allowed_states=[State.SKIPPED],
         execution_delta=timedelta(hours=6),
         poke_interval=10
